e3163d48c6d0f242f1bc9ff4a0f5fc11088b665a
Louis George <louisg495@gmail.com>
2020-12-20 23:27:00 -0800

    started depth line and translate func.

--- main.py
+++ main.py
@@ -24,32 +24,37 @@ class MainScatter(Scatter):
     end_point_1_top_prop = ListProperty([])
     end_point_2_top_prop = ListProperty([])
     depth_point_top_prop = ListProperty([])
-    intercept_point_top_prop = ListProperty([])
+    depth_point_intercept_top_prop = ListProperty([])
 
     end_point_1_mid_prop = ListProperty([])
     end_point_2_mid_prop = ListProperty([])
     depth_point_mid_prop = ListProperty([])
-    intercept_point_mid_prop = ListProperty([])
+    depth_point_intercept_mid_prop = ListProperty([])
 
     end_point_1_btm_prop = ListProperty([])
     end_point_2_btm_prop = ListProperty([])
     depth_point_btm_prop = ListProperty([])
-    intercept_point_btm_prop = ListProperty([])
+    depth_point_intercept_btm_prop = ListProperty([])
 
     def on_end_point_1_top_prop(self, instance, value):
         for child in self.children:
             if hasattr(child, "name") and child.name == "main_line_top":
                 child.update_line(1, value)
+            elif hasattr(child, "name") and child.name == "depth_point_top":
+                child.translate_point(1, value)
 
     def on_end_point_2_top_prop(self, instance, value):
         for child in self.children:
             if hasattr(child, "name") and child.name == "main_line_top":
                 child.update_line(2, value)
+            elif hasattr(child, "name") and child.name == "depth_point_top":
+                child.translate_point(2, value)
 
-    # def depth_point_top_prop(self, instance, value):
-    #     for child in self.children:
-    #         if hasattr(child, "name") and child.name == "depth_line_top":
-    #             child.update_line(2, value)
+    def depth_point_top_prop(self, instance, value):
+        for child in self.children:
+            if hasattr(child, "name") and child.name == "depth_line_top":
+                pass
+                # child.update_line(2, value)
 
     def on_end_point_1_mid_prop(self, instance, value):
         for child in self.children:
@@ -184,6 +189,9 @@ class DepthPoint(Widget):
     def update_point(self, *args):
         self.outer.pos = self.pos
         self.inner.pos = (self.pos[0] + 3, self.pos[1] + 3)
+
+    def translate_point(self, *args):
+
 
     def on_touch_down(self, touch):
         if self.collide_point(*touch.pos):
@@ -410,6 +418,14 @@ class MainLine(Widget):
     #     touch.ungrab(self)
     #     return True
 
+class DepthLine(MainLine):
+    def update_line(self, point, value):
+        if point == 1:
+            self.outer.points = value + self.outer.points[2:]
+            self.inner.points = value + self.outer.points[2:]
+        elif point == 2:
+            self.outer.points = self.outer.points[:2] + value
+            self.inner.points = self.outer.points[:2] + value
 
 class MainMenuScreen(Screen):
     pass
@@ -451,12 +467,15 @@ class SplineScreen(Screen):
             end_point_1_coords = (win.width * 0.25, win.height * 0.50)
             end_point_2_coords = (win.width * 0.75, win.height * 0.50)
             depth_point_coords = (win.width * 0.50, win.height * 0.25)
+            depth_point_intercept_coords = (win.width * 0.50, win.height * 0.50)
             # Instantiating the chord elements
-            main_line = MainLine(name=f"main_line_{btn_name}", points=list(end_point_1_coords+end_point_2_coords))
+            depth_line = DepthLine(name=f"depth_line_{btn_name}", points=list(depth_point_intercept_coords + depth_point_coords))
+            main_line = MainLine(name=f"main_line_{btn_name}", points=list(end_point_1_coords + end_point_2_coords))
             end_point_1 = EndPoint(name=f"end_point_1_{btn_name}", center=end_point_1_coords) # pos_hint={"center_x":win.width * 0.25 / win.width, "center_y": win.height / 2 / win.height})
             end_point_2 = EndPoint(name=f"end_point_2_{btn_name}", center=end_point_2_coords) # pos_hint={"center_x":win.width * 0.25 / win.width, "center_y": win.height / 2 / win.height}) #
             depth_point = DepthPoint(name=f"depth_point_{btn_name}", center=depth_point_coords)
             # Adding chord elements to scatter
+            self.ids.scatter.add_widget(depth_line)
             self.ids.scatter.add_widget(main_line)
             self.ids.scatter.add_widget(end_point_1)
             self.ids.scatter.add_widget(end_point_2)
@@ -466,14 +485,17 @@ class SplineScreen(Screen):
                 self.ids.scatter.end_point_1_top_prop = end_point_1_coords
                 self.ids.scatter.end_point_2_top_prop = end_point_2_coords
                 self.ids.scatter.depth_point_top_prop = depth_point_coords
+                self.ids.scatter.depth_point_intercept_top_prop = depth_point_coords
             elif btn_name == "mid":
                 self.ids.scatter.end_point_1_mid_prop = end_point_1_coords
                 self.ids.scatter.end_point_2_mid_prop = end_point_2_coords
                 self.ids.scatter.depth_point_mid_prop = depth_point_coords
+                self.ids.scatter.depth_point_intercept_mid_prop = depth_point_coords
             elif btn_name == "btm":
                 self.ids.scatter.end_point_1_btm_prop = end_point_1_coords
                 self.ids.scatter.end_point_2_btm_prop = end_point_2_coords
                 self.ids.scatter.depth_point_btm_prop = depth_point_coords
+                self.ids.scatter.depth_point_intercept_btm_prop = depth_point_coords
 
 
 class SM(ScreenManager):
