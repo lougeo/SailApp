c43a0e98a3038c6ae127fecd9b3903da031d521f
Louis George <louisg495@gmail.com>
2021-01-02 14:23:10 -0800

    more perms

--- main.py
+++ main.py
@@ -24,6 +24,7 @@ import time
 import math
 
 if platform == "android":
+    # from android.storage import primary_external_storage_path
     from android.permissions import request_permissions, Permission
     request_permissions([
         Permission.CAMERA,
