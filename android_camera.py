ede6f4042751d1f1870a78d441bf8456cdf35df6
Louis George <louisg495@gmail.com>
2021-01-13 23:53:58 -0800

    camera

--- android_camera.py
+++ android_camera.py
@@ -8,7 +8,8 @@ PythonActivity = autoclass('org.kivy.android.PythonActivity')
 MediaStore = autoclass('android.provider.MediaStore')
 Uri = autoclass('android.net.Uri')
 FileProvider = autoclass('android.support.v4.content.FileProvider')
-
+Context = autoclass("android.content.Context")
+Environment = autoclass("android.os.Environment")
 
 class AndroidCamera:
 
@@ -21,19 +22,22 @@ class AndroidCamera:
         intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
         # uri = Uri.parse('file:/' + filename)
         # print(f"URI: {uri}")
-        fileprovider = FileProvider('content:/' + filename)
-        parcelable = cast('android.os.Parcelable', fileprovider)
+        # uri = FileProvider.getUriForFile(
+        #     Context.getApplicationContext(),
+        #     "com.sailmeter.sailapp.fileprovider",
+        #     filename,
+        # )
+        uri = "content://com.sailmeter.fileprovider" + filename
+        parcelable = cast('android.os.Parcelable', filename)
         print(f"PARCELABLE: {parcelable}")
         intent.putExtra(MediaStore.EXTRA_OUTPUT, parcelable)
         PythonActivity.mActivity.startActivityForResult(intent, 0x123)
 
     def _on_activity_result(self, requestCode, resultCode, intent):
         if requestCode != 0x123:
-            "REQUEST CODE INCORRECT"
             return
         android.activity.unbind(on_activity_result=self._on_activity_result)
         if self.on_complete(self.filename):
-            print("TRIGGERING REMOVE")
             self._remove(self.filename)
 
     def _remove(self, fn):
