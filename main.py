937e8106dd39a58cccca9c44f2286a1e4fa5325d
Louis George <louisg495@gmail.com>
2021-01-15 14:40:29 -0800

    image

--- main.py
+++ main.py
@@ -877,7 +877,7 @@ class MainMenuScreen(Screen):
         if(exists(filepath)):
             print("PICTURE SAVED")
             print(filepath)
-            partial_path = filepath.replace("/storage/emulated/0/", "")
+            partial_path = filepath.replace("/storage/emulated/0", "")
             self.manager.transition.direction = "left"
             self.manager.current = "spline_screen"
             self.manager.get_screen('spline_screen').img_src = partial_path
