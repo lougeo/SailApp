ca8ec4c5799c3c7d7380f13b4e30121a95fcf199
Louis George <louisg495@gmail.com>
2021-02-07 17:39:19 -0800

    cleaned up imports

--- main.py
+++ main.py
@@ -5,27 +5,19 @@ kivy.require("2.0.0")
 from kivy.clock import Clock
 from kivy.core.window import Window
 from kivy.lang import Builder
-from kivy.uix.layout import Layout
-from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.gridlayout import GridLayout
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.scatterlayout import ScatterLayout
-from kivy.uix.label import Label
-from kivy.uix.button import Button
 from kivy.uix.widget import Widget
 from kivy.uix.scatter import Scatter
-from kivy.uix.screenmanager import ScreenManager, Screen, SlideTransition
+from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.camera import Camera
-from kivy.uix.image import Image
 from kivy.utils import platform
 from kivy.properties import (
-    ObjectProperty,
     StringProperty,
     ListProperty,
     BooleanProperty,
 )
 
-from kivy.graphics import Color, Rectangle, Point, Line, Ellipse, Bezier
+from kivy.graphics import Color, Rectangle, Line, Bezier
 
 import json
 import os
